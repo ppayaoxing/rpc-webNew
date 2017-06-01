c0b4943b8631fbf1a0db51d8472149c17e3be437
jaco <huangzhixion51@126.com>
2017-06-01 11:37:39 +0800

    提交sql

---
+++ rpc_web.sql
@@ -0,0 +1,24 @@
+/*
+SQLyog Ultimate v11.27 (32 bit)
+MySQL - 5.6.25 : Database - rpc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rpc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+/*Data for the table `task_data` */
+
+insert  into `task_data`(`id`,`biz_code`,`fullClass`,`createTime`,`corn`,`name`,`groupName`,`triggerName`,`updateTime`,`status`) values (1,'test','com.xw.task.TestTask','2017-04-24 10:17:53','0/5 * * * * ?','test','test','test','2017-04-24 11:06:59',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
